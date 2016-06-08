3b2fd481e8da9ab55ec69a26feab9edbec97b52c
Albin Vass <wass95@hotmail.se>
2016-06-08 22:31:14 +0200

    working

--- asstobutt.js
+++ asstobutt.js
@@ -1,1 +1,16 @@
-document.body.innerHTML = document.body.innerHTML.replace(/ass/g, 'butt');
+function newTheOlds(node) {
+    node = node || document.body;
+    if(node.nodeType == 3) {
+        // Text node
+        node.nodeValue = node.nodeValue.replace(/a[Ss]{2}/g,'butt');
+        node.nodeValue = node.nodeValue.replace(/A[Ss]{2}/g,'Butt');
+        node.nodeValue = node.nodeValue.replace(/A\.S\.S/g,'B.U.T.T');
+    } else {
+        var nodes = node.childNodes;
+        if(nodes) {
+            var i = nodes.length;
+            while(i--) newTheOlds(nodes[i]);
+        }
+    }
+}
+document.addEventListener("DOMContentLoaded", newTheOlds()); 
