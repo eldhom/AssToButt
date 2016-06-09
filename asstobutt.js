cee8eac195975678dcb17607195b30d3afb74220
Albin Vass <wass95@hotmail.se>
2016-06-09 13:28:59 +0200

    Easier to use

--- asstobutt.js
+++ asstobutt.js
@@ -1,16 +1,43 @@
-function assToButt(node) {
-    node = node || document.body;
-    if(node.nodeType == 3) {
-        // Text node
-        node.nodeValue = node.nodeValue.replace(/a[Ss]{2}/g,'butt');
-        node.nodeValue = node.nodeValue.replace(/A[Ss]{2}/g,'Butt');
-        node.nodeValue = node.nodeValue.replace(/A\.S\.S/g,'B.U.T.T');
-    } else {
-        var nodes = node.childNodes;
-        if(nodes) {
-            var i = nodes.length;
-            while(i--) assToButt(nodes[i]);
-        }
-    }
+function replaceAss(word) {
+	if(word.charAt(0) == word.charAt(0).toUpperCase()) {
+		return "Butt";
+	} 
+	return "butt";
 }
-document.addEventListener("DOMContentLoaded", assToButt()); 
+
+/*
+*	Associative map: 
+*	add lowercase words associated to a function 
+*	which takes a regex matched word as argument
+*/
+var replaceMap 	= {
+	"ass":replaceAss
+}
+
+/*
+*	List of regex to be matched
+*/
+var regexArr 	= [
+	"[Aa][Ss]{2}"
+];
+
+
+function regexReplace(node) {
+	node = node || document.body;
+	if(node.nodeType == 3) {
+		// Text node
+		var re = new RegExp("("+regexArr.join("|")+")", "gi");
+				console.log(re);
+		node.nodeValue = node.nodeValue.replace(re, function(matched, p1) {
+				return replaceMap[matched.toLowerCase()](p1);
+				});
+	} else {
+		var nodes = node.childNodes;
+		if(nodes) {
+			for(var i=0; i<nodes.length; i++) {
+				regexReplace(nodes[i]);
+			}
+		}
+	}
+}
+document.addEventListener("DOMContentLoaded", regexReplace()); 
